050d1f1c267ca8ed81f2fd30a45466354d8c9c41
David Boike <david.boike@gmail.com>
2023-03-21 16:25:13 -0500

    Fail when no tests found

--- run-tests.ps1
+++ run-tests.ps1
@@ -82,10 +82,10 @@ foreach ($framework in $testFrameworks) {
         $targetPlatformParam = "RunConfiguration.TargetPlatform=$($Env:TARGET_PLATFORM)"
 
         if ($filter -ne '') {
-           dotnet test $project.Name --configuration Release --no-build --filter "$filter" --framework $framework --logger "GitHubActions;report-warnings=$reportWarnings" -- $targetPlatformParam
+           dotnet test $project.Name --configuration Release --no-build --filter "$filter" --framework $framework --logger "GitHubActions;report-warnings=$reportWarnings" -- RunConfiguration.TreatNoTestsAsError=true $targetPlatformParam 
         }
         else {
-            dotnet test $project.Name --configuration Release --no-build --framework $framework --logger "GitHubActions;report-warnings=$reportWarnings" -- $targetPlatformParam
+            dotnet test $project.Name --configuration Release --no-build --framework $framework --logger "GitHubActions;report-warnings=$reportWarnings" -- RunConfiguration.TreatNoTestsAsError=true $targetPlatformParam
         }
         
 
