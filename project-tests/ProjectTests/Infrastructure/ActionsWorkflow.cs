d028e8cd832f0129966f471d0a8bd3b05e4d898c
David Boike <david.boike@gmail.com>
2024-02-11 15:57:02 -0600

    Compare test project TFMs with actions/setup-dotnet's versions

---
+++ project-tests/ProjectTests/Infrastructure/ActionsWorkflow.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+using YamlDotNet.Serialization;
+
+namespace ProjectTests.Infrastructure
+{
+    public class ActionsWorkflow
+    {
+        JsonNode root;
+
+        public ActionsWorkflow(string path)
+        {
+            var deserializer = new Deserializer();
+            var yamlObject = deserializer.Deserialize(File.ReadAllText(path));
+
+            var json = JsonSerializer.Serialize(yamlObject, new JsonSerializerOptions { WriteIndented = true });
+            root = JsonSerializer.Deserialize<JsonNode>(json);
+
+            var options = new JsonSerializerOptions()
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower
+            };
+
+            Name = root["name"].GetValue<string>();
+            RunName = root["run-name"]?.GetValue<string>();
+            On = root["on"].AsObject();
+            Permissions = JsonSerializer.Deserialize<IReadOnlyDictionary<string, string>>(root["permissions"]);
+            Env = JsonSerializer.Deserialize<IReadOnlyDictionary<string, string>>(root["env"]);
+
+            Jobs = root["jobs"].AsObject().Select(pair =>
+            {
+                var job = JsonSerializer.Deserialize<WorkflowJob>(pair.Value, options);
+                job.Id = pair.Key;
+                return job;
+            })
+            .ToArray();
+        }
+
+        public string Name { get; }
+        public string RunName { get; }
+        public JsonObject On { get; }
+        public IReadOnlyDictionary<string, string> Permissions { get; }
+        public IReadOnlyDictionary<string, string> Env { get; }
+
+        public WorkflowJob[] Jobs { get; }
+    }
+
+    public class WorkflowJob
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        [JsonPropertyName("runs-on")]
+        public string RunsOn { get; set; }
+        public JsonObject Strategy { get; set; }
+        public JobStep[] Steps { get; set; }
+    }
+
+    public class JobStep
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string If { get; set; }
+        public string Shell { get; set; }
+        public IReadOnlyDictionary<string, string> Env { get; set; }
+        public string Run { get; set; }
+        public string Uses { get; set; }
+        public IReadOnlyDictionary<string, string> With { get; set; }
+    }
+}
--- project-tests/ProjectTests/TestProjects.cs
+++ project-tests/ProjectTests/TestProjects.cs
@@ -1,10 +1,19 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.RegularExpressions;
 using System.Xml.XPath;
+using Microsoft.VisualBasic;
 using NUnit.Framework;
+using ProjectTests.Infrastructure;
+using YamlDotNet.Serialization;
 
 namespace ProjectTests
 {
-    public class TestProjects
+    public partial class TestProjects
     {
         [Test]
         public void ValidateProjectFrameworks()
@@ -15,18 +24,89 @@ namespace ProjectTests
                 Assert.Ignore("No ci.yml workflow found in root directory");
             }
 
+            var workflow = new ActionsWorkflow(ciPath);
+
+            var ciNetVersions = workflow.Jobs
+                .SelectMany(j => j.Steps.Where(s => s.Uses?.StartsWith("actions/setup-dotnet@") ?? false))
+                .Select(step => step.With["dotnet-version"])
+                .Select(versions => Regex.Split(versions, @"(\r?\n)+").Where(s => !string.IsNullOrWhiteSpace(s)).ToArray())
+                .ToArray();
+
+            // If workflow has more than one job, make sure someone didn't update one setup-dotnet and forget the other one
+            for (var i = 1; i < ciNetVersions.Length; i++)
+            {
+                Assert.That(ciNetVersions[0], Is.EquivalentTo(ciNetVersions[i]), "All the .NET versions requested by jobs in ci.yml should be the same");
+            }
+
+            var expectedFrameworks = ciNetVersions[0].Select(DotNetVersionToTargetFramework).ToArray();
+
+            var collectedTestFrameworks = new List<(string path, string frameworks)>();
+
             new TestRunner("*.csproj", "Find tests")
                 .SdkProjects()
                 .TestProjects()
                 .Run(file =>
                 {
                     var frameworksText = file.XDocument.XPathSelectElement("/Project/PropertyGroup/TargetFramework")?.Value
-                        ?? file.XDocument.XPathSelectElement("/Project/PropertyGroup/TargetFramework")?.Value;
+                        ?? file.XDocument.XPathSelectElement("/Project/PropertyGroup/TargetFrameworks")?.Value;
+
+                    collectedTestFrameworks.Add((file.FilePath, frameworksText));
 
                     var frameworks = frameworksText.Split(';');
 
-                    file.Fail(frameworksText);
+                    if (!frameworks.All(tfm => tfm.StartsWith("net4") || expectedFrameworks.Contains(tfm)))
+                    {
+                        file.Fail("Target frameworks don't match the dotnet-versions in the ci.yml workflow");
+                    }
                 });
+
+            var groups = collectedTestFrameworks.GroupBy(x => x.frameworks)
+                .OrderBy(g => g.Count())
+                .ToArray();
+
+            if (groups.Length > 1)
+            {
+                var msg = new StringBuilder().AppendLine("The target frameworks of the test projects do not all match:");
+
+                foreach (var g in groups)
+                {
+                    msg.AppendLine($"  * Target Frameworks: '{g.Key}':");
+                    foreach (var proj in g)
+                    {
+                        msg.AppendLine($"    * {proj.path}");
+                    }
+                }
+
+                Assert.Fail(msg.ToString());
+            }
         }
+
+        string DotNetVersionToTargetFramework(string dotnetVersion)
+        {
+            var result = dotnetVersion switch
+            {
+                "2.1.x" => "netcoreapp2.1",
+                "3.1.x" => "netcoreapp3.1",
+                _ => null
+            };
+
+            if (result != null)
+            {
+                return result;
+
+            }
+
+            var match = DotNetVersionRegex().Match(dotnetVersion);
+            if (match.Success)
+            {
+                var dotnetMajor = match.Groups[1].Value;
+                return $"net{dotnetMajor}.0";
+            }
+
+            throw new System.Exception($"Unable to map dotnet-version value (i.e. '8.0.x') to target framework (i.e. 'net8.0'. A mapping for the value '{dotnetVersion}' may be missing, or it may be incorrect.");
+        }
+
+        [GeneratedRegex(@"(\d+)\.0\.x")]
+        private static partial Regex DotNetVersionRegex();
     }
 }
